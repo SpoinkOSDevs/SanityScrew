58d377a380e65c62ca7df0b76314f978d5c7dea2
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2024-06-21 12:08:16 -0400

    Update SanityScrew.cpp

--- src/SanityScrew.cpp
+++ src/SanityScrew.cpp
@@ -1,13 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
 #include "SSJIT.h"
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <input_file> <output_file>" << std::endl;
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_file.ss>\n";
+        return 1;
+    }
+
+    std::string inputFile = argv[1];
+    std::ifstream file(inputFile);
+    if (!file.is_open()) {
+        std::cerr << "Error: Unable to open input file " << inputFile << "\n";
         return 1;
     }
 
     std::vector<std::string> code;
-    std::ifstream file(argv[1]);
     std::string line;
     while (std::getline(file, line)) {
         code.push_back(line);
